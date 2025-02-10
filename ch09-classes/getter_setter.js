55479bfa5e0dd5dc805e3e58542fc14cb51f3e57
ysoma <yudaisoma@gmail.com>
2025-02-10 22:57:18 +0900

    クラスとメソッドの追加: Rangeクラスにstaticメソッドparseを実装し、getter/setterメソッドのサンプルを追加

---
+++ ch09-classes/getter_setter.js
@@ -0,0 +1,7 @@
+// Date: 2025/02/10
+// Creator: ysoma
+// Ref: JavaScript: The Definitive Guide, 7th Edition
+// Chapter 9. Class
+// ------------------------------------------------
+
+// 9.3.2 ゲッターメソッドやセッターメソッドなど、他のメソッド形式
