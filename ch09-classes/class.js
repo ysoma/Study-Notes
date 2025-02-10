7e4fe0967cf8cf771c901b49d3caca074186c8ed
ysoma <yudaisoma@gmail.com>
2025-02-10 19:50:11 +0900

    Rangeクラスに降順対応のイテレータを追加し、Spanクラスを実装

--- ch09-classes/class.js
+++ ch09-classes/class.js
@@ -10,19 +10,55 @@ class Range {
     constructor(from, to) {
         this.from = from;
         this.to = to;
+        this.length = to - from;
     }
 
     includes(x) { return this.from <= x && x <= this.to; }
 
+    // 本のコードでは、降順なrangeに対しては未対応
+    // 一旦、超愚直なやり方で書いてみる。
     *[Symbol.iterator]() {
-        for (let x = Math.ceil(this.from); x <= this.to; x++) yield x;
+        if (this.from < this.to) {
+            for (let x = Math.ceil(this.from); x <= this.to; x++) {
+                yield x;
+            }
+        }
+        else {
+            for (let x = Math.ceil(this.from); x >= this.to; x--) {
+                yield x;
+            }
+        }
     }
 
     toString() { return `(${this.from}...${this.to})`; }
 }
 
+// クラスの継承
+// SpanはRangeに似たクラス。
+// Spanは開始位置と長さを指定して範囲を指定する。
+class Span extends Range {
+    constructor(start, length) {
+        if (length >= 0) {
+            super(start, start + length);
+        } else {
+            super(start + length, start);
+        }
+    }
+}
+
 // 以下はRangeオブジェクトを使った例
-let r = new Range(1, 3); // Rangeオブジェクトを生成
-r.includes(2); // => true: 2は範囲内である
-r.toString(); // => "(1...3)"
-[...r]; // => [1, 2, 3]: Rangeオブジェクトは反復可能である
+let range = new Range(0, -10); // startからendまでの範囲を表すオブジェクト
+let span = new Span(-1, -3); // startから始まる長さlengthの範囲を表すオブジェクト
+
+
+console.log("[toString()]");
+console.log("range.toString():", range.toString());
+console.log("span.toString():", span.toString());
+
+console.log("[length]");
+console.log("range.length:", range.length);
+console.log("span.length:", span.length);
+
+console.log("[iterator]");
+console.log("range iterator:", [...range]);
+console.log("span iterator", [...span]);
